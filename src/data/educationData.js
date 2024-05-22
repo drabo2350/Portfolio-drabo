af1f0d76ddaa25c981e92e4c50f1c918c7a2f006
lacina <machine9698@gmail.com>
2024-05-22 17:16:12 +0200

    modification 4

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,22 +1,22 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Lycée moderne de boignykro',
-        course: 'DIPLOME 3',
-        startYear: '2021',
+        institution: 'Université Virtuelle de Côte d\'Ivoire',
+        course: 'Licence - Developpement d\'application',
+        startYear: '2023',
         endYear: 'Present'
     },
     {
         id: 2,
-        institution: 'Lycée moderne de boignykro',
-        course: 'DIPLOME 2',
+        institution: 'Lycée moderne HKB Sinfra',
+        course: 'Baccalauréat Scientifique (C) ',
         startYear: '2018',
         endYear: '2019'
     },
     {
         id: 3,
-        institution: 'Lycée moderne de boignykro',
-        course: 'DIPLOME 1',
+        institution: 'Lycée Municipal Sinfra',
+        course: 'BEPC',
         startYear: '2015',
         endYear: '2016'
     },
