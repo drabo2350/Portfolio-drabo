a8d6039e28da2ecfdfd2527321e6cf688e7b69e3
lacina <machine9698@gmail.com>
2024-05-22 18:19:32 +0200

    modification

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,32 +1,51 @@
 import community from '../assets/jpg/manager.jpg'
+import gestionProjet from '../assets/png/gestion.png'
+import communicationDigitale from '../assets/jpg/com-digi.jpg'
+import modelisation3d from '../assets/png/3d.png'
+import html from '../assets/png/html.png'
 export const achievementData = {
     bio : "Professionnel polyvalent et expérimenté dans le domaine du marketing numérique, avec une solide expérience dans la gestion de campagnes en ligne et hors ligne. Expert en stratégies de contenu et en communication, ayant dirigé des équipes pour atteindre des objectifs ambitieux. Passionné par l'innovation et la créativité, je cherche à relever de nouveaux défis dans un environnement dynamique.",
     achievements : [
         {
-            id : 1,
-            title : 'Certification Google Cloud',
-            details : 'Les certifications Google Cloud valident votre expertise et mettent en valeur votre capacité à transformer les entreprises avec la technologie Google Cloud.',
-            date : 'Sept 20, 2019',
-            field : 'Big DATA',
-            image : community
-        },
-        {
             id : 2,
-            title : 'Certification AWS',
-            details : 'Validez vos compétences techniques et votre expertise cloud pour développer votre carrière et votre entreprise.',
-            date : 'Mai 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'Gestion de Projet Informatique',
+            details : 'Coordination et exécution de projets informatiques, incluant la gestion des ressources, la maîtrise des risques, et l\'assurance qualité pour atteindre les objectifs fixés.',
+            date : 'Mai 2023',
+            field : 'UVCI',
+            image : gestionProjet
+        }, 
+        {
+            id : 3,
+            title : 'Communication digitale',
+            details : 'Stratégies de marketing en ligne, gestion des réseaux sociaux, création de contenus numériques, et analyse des performances pour optimiser la présence digitale des entreprises.',
+            date : 'Janvier 2023',
+            field : 'UVCI',
+            image : communicationDigitale
         },
         {
             id : 3,
-            title : 'Certification professionnelle d\'ingénieur de données',
-            details : 'Les ingénieurs de données professionnels permettent une prise de décision basée sur les données en collectant, transformant et publiant des données.',
-            date : 'Janvier 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'Modelisation 3D',
+            details : 'Modèles numériques précis et réalistes, appliqués aux domaines du design, de l\'animation et de la visualisation architecturale.',
+            date : 'Novembre 30, 2022',
+            field : 'Orange Digital',
+            image : modelisation3d
+        },
+        {
+            id : 3,
+            title : 'L\'essentiel du HTML5',
+            details : 'Les Fondamentaux du développement web, incluant la création de pages structurées, accessibles et optimisées pour différents navigateurs et appareils.',
+            date : 'Octobre 2022',
+            field : 'Linkedin Learning',
+            image : html
+        },
+        {
+            id : 1,
+            title : 'Community Management',
+            details : 'Développement de stratégies de contenu, engagement des utilisateurs, et analyse des performances pour renforcer la présence et l\'interaction sur les réseaux sociaux.',
+            date : 'Mars 2020',
+            field : 'UVCI',
+            image : community
         }
-
     ]
 }
 
