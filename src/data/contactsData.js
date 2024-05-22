388a68ad716a376630088c2f08caecf7c533a79c
lacina <machine9698@gmail.com>
2024-05-22 16:17:09 +0200

    Le portforlio v1

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'buseruka@mail.com',
+    phone: '+2250000000000',
+    address: 'Abidjan, Cocody',
+
+    sheetAPI: ''
+}
---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import resume from '../assets/pdf/resume.pdf'
+import sangare from '../assets/jpg/sangaresuit.png'
+
+export const headerData = {
+    name: 'Hamed SANGARE',
+    title: "Marketing et communication",
+    desciption:"La programmation, c'est comme une symphonie. Il faut que tous les instruments jouent ensemble pour créer quelque chose d'extraordinaire.\nChaque grand projet a commencé par une ligne de code.",
+    image: sangare,
+    resumePdf: resume
+}
