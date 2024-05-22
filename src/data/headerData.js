e8a857cc6b6130e83a7b364de193e2134a5ac791
lacina <machine9698@gmail.com>
2024-05-22 16:56:12 +0200

    modification 2

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
-import profil from '../assets/jpg/lacina.png'
+import lacina from '../assets/jpg/lacina.png'
 
 export const headerData = {
     name: 'Lacina DRABO',
     title: "Developpeur React",
     desciption:"La programmation, c'est comme une symphonie. Il faut que tous les instruments jouent ensemble pour créer quelque chose d'extraordinaire.\nChaque grand projet a commencé par une ligne de code.",
-    image: profil,
+    image: lacina,
     resumePdf: resume
 }
